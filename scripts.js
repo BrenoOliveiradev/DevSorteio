cb2c257134918d15144a533e414f554cdb24ebd0
BrenoOliveiradev <brenooliveira.dev@hotmail.com>
2025-06-22 19:46:02 -0300

    att do projeto , validação de entrada, setTimeout nos inputs

--- scripts.js
+++ scripts.js
@@ -1,9 +1,37 @@
-function generateNumber(){
+const button = document.querySelector("button");
+const inputMin = document.querySelector(".input-min");
+const inputMax = document.querySelector(".input-max");
+const resultado = document.querySelector(".resultado");
 
- const min = Math.ceil(document.querySelector(".input-min").value)
- const max = Math.floor(document.querySelector(".input-max").value)
- 
- const result = Math.floor(Math.random() * (max - min + 1)) + min;
- 
- console.log(min,max)
-}
+button.addEventListener("click", () => {
+    const min = Math.ceil(Number(inputMin.value));
+    const max = Math.floor(Number(inputMax.value));
+
+    if (inputMin.value === "" || inputMax.value === "") {
+        resultado.textContent = "⚠️ Por favor, preencha os dois campos!";
+        resultado.style.color = "#d62828";
+        return;
+    }
+
+    if (isNaN(min) || isNaN(max)) {
+        resultado.textContent = "⚠️ Valores inválidos!";
+        resultado.style.color = "#d62828";
+        return;
+    }
+
+    if (min >= max) {
+        resultado.textContent = "⚠️ O número mínimo deve ser menor que o máximo.";
+        resultado.style.color = "#d62828";
+        return;
+    }
+
+    const numeroSorteado = Math.floor(Math.random() * (max - min + 1)) + min;
+    resultado.innerHTML = `🎉 Número sorteado: <span style="color:rgb(0, 0, 0); font-weight: bold;">${numeroSorteado}</span>`;
+    resultado.style.color = "#1b4b5a";
+
+
+    setTimeout(() => {
+        inputMin.value = "";
+        inputMax.value = "";
+    }, 10000);
+});
